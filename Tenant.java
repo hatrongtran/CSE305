b336f5d8930dd3984e3a90371289d7f06617b8b7
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-04-23 15:54:43 +0700

    Loc's ver

---
+++ Tenant.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tenant {
+    public User user;
+    private List<RentalContractandLease> ListOfContract = new ArrayList<>();
+
+}
+
